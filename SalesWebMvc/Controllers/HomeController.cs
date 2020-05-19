f4fefd3338ea8260c4267df00c8b8457354bd1aa
Rafael Enrique <rafaelenrique30@gmail.com>
2020-05-19 15:47:48 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["email"] = "rafael@gmail.com";
 
             return View();
         }
